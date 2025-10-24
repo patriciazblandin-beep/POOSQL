12205871f89c21ba521da2fc2ed58dd2fee3231f
PatriciaB <patriciazblandin@gmail.com>
2025-10-24 01:04:39 -0600

    orden de ejecucion de sql

--- FirstStep.sql
+++ FirstStep.sql
@@ -9,6 +9,8 @@ CREATE TABLE academics.studentss(
 )
 SELECT * from academics.studentss
 
+
+---jemplo de insercion exitosa
 INSERT into academics.studentss (
     firstname
     , lastname
@@ -17,7 +19,56 @@ INSERT into academics.studentss (
 ) VALUES (
     ' Jasmin'
     , 'Zelaya'
-    , '0800'
+    , '0801'
     , 'jaszzz@gmil.com'
 
 )
+
+
+INSERT into academics.studentss (
+    firstname
+    , lastname
+    ,idperson
+    ,email
+    ,age
+) VALUES (
+    ' Gabriela'
+    , 'Garcia'
+    , '0815'
+    , 'gaby@gmil.com'
+    , 18
+
+)
+---modificar la estructura de la tabla agregando una nueva columna
+ALTER TABLE academics.studentss ADD age int null
+
+
+SELECT * from academics.studentss
+
+---ejemplo de actualizacion exitosa 
+UPDATE academics.studentss
+SET firstname = 'Patri', lastname = 'Blandin' , email = 'patri@gamil.com', idperson = '0800'
+WHERE id = 2
+
+---ejemplo para actualizar todos los registros
+UPDATE academics.studentss
+SET age  = 20
+
+---actualizar el 'age' es 20,  donde el lastname es = zelaya
+UPDATE academics.studentss 
+set age = 30 
+WHERE lastname ='Zelaya'
+
+---ejemplo para eliminar un registro con id = 3
+DELETE FROM academics.studentss
+where id =3
+
+---seleccionar a todas los estudiantes menores de 20 años
+
+SELECT
+id 
+, lastname
+, email
+, age
+FROM academics.studentss
+WHERE age < 21
